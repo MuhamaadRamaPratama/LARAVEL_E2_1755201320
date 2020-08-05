cca42b28f0d552526865546515d44f2782bf3db3
MuhammadRamaPratama <ramap239@gmail.com>
2020-08-05 19:59:00 +0700

    Praktek Pertemuan 12 : CRUD

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mahasiswa;
+use App\Prodi;
 use DataTables;
 use Illuminate\Http\Request;
 
@@ -16,16 +17,17 @@ class MahasiswaController extends Controller
     public function index()
     {
         return view('mahasiswa.index');
-        //$mhs = Mahasiswa::where('nim', '1755201320')->first();
-
-        //return $mhs->nama_lengkap;
     }
 
     public function mhs_list()
     {
-        return Datatables::of(Mahasiswa::all())
-        ->addIndexColumn()
-        ->make(true);
+        $mhs = Mahasiswa::with('mprodi')->get();
+        return Datatables::of($mhs)->addIndexColumn()->addColumn('action', function ($mhs){
+                    $action = '<a class="text-primary"href="/mhs/edit/'.$mhs->nim.'">Edit</a>';
+                    $action .= ' | <a class="text-danger"href="/mhs/delete/'.$mhs->nim.'">Hapus</a>';
+                    return $action;
+               })->make();
+
     }
 
     /**
@@ -35,7 +37,8 @@ class MahasiswaController extends Controller
      */
     public function create()
     {
-        //
+         $prodi = Prodi::all();
+        return view('mahasiswa.create', compact('prodi'));
     }
 
     /**
@@ -46,7 +49,13 @@ class MahasiswaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         $request->validate([
+            'nim' => 'required|digits:10',
+            'nama_lengkap' => 'required',
+       ]);
+         Mahasiswa::create($request->all());
+        return redirect()->route('mhs.index')
+                        ->with('success','Data berhasil ditambahkan');
     }
 
     /**
@@ -66,9 +75,11 @@ class MahasiswaController extends Controller
      * @param  \App\Mahasiswa  $mahasiswa
      * @return \Illuminate\Http\Response
      */
-    public function edit(Mahasiswa $mahasiswa)
+    public function edit(Mahasiswa $mahasiswa, $id)
     {
-        //
+         $prodi = Prodi::all();
+        $mhs = Mahasiswa::find($id);
+        return view('mahasiswa.edit', compact('prodi', 'mhs'));
     }
 
     /**
@@ -80,7 +91,12 @@ class MahasiswaController extends Controller
      */
     public function update(Request $request, Mahasiswa $mahasiswa)
     {
-        //
+         $request->validate([
+            'nama_lengkap' => 'required',
+       ]);
+        $mahasiswa->update($request->all());
+        return redirect()->route('mhs.index')
+                        ->with('success','Data berhasil diupdate');
     }
 
     /**
@@ -91,6 +107,8 @@ class MahasiswaController extends Controller
      */
     public function destroy(Mahasiswa $mahasiswa)
     {
-        //
+        $mahasiswa->delete();
+         return redirect()->route('mhs.index')
+         ->with('success','Data Berhasil Dihapus');
     }
 }
---
+++ resources/views/prodi/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('title', 'Prodi Page')
+@section('bread1', 'Prrogram Studi')
+@section('bread2', 'Data')
+@section('content')
+	<h1>Master Data Program Studi</h1>
+	<table class="table table-striped " id="mhs-table" >
+		<thead>
+				<th>No</th>
+				<th>Kode Prodi</th>
+				<th>Nama Prodi</th>
+				<th>Kaprodi</th>
+		</thead>
+		<tbody>
+			@foreach($list_prodi as $key => $item)
+			<tr>
+				<td>{{$key+1}}</td>
+				<td>{{$item->kode_prodi}}</td>
+				<td>{{$item->nama_prodi}}</td>
+				<td>{{$item->kaprodi}}</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	
+@endsection
